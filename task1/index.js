291df26a3d779d0f3ef3b343d09d76d142fb87e1
anton_yatskevich <anton_yatskevich@mail.ru>
2020-08-03 22:36:21 +0300

    feat: second solution for task1

--- task1/index.js
+++ task1/index.js
@@ -1,10 +1,35 @@
-const readline = require('readline');
+const { Transform } = require('stream');
 
-const readlineStream = readline.createInterface({
-  input: process.stdin,
-});
+class ReverseTransformer extends Transform {
+  _transform(data, encoding, cb) {
+    this.push(
+      data
+        .toString()
+        .split('')
+        .reverse()
+        .concat('\n\n')
+        .join('')
+    )
+    cb()
+  }
+}
+ 
+const read = process.stdin;
+const write = process.stdout;
+const Transformer = new ReverseTransformer();
 
-readlineStream.on('line', (input) => {
-  const reversedString = input.split('').reverse().join('');
-  console.log(reversedString, '\n');
-});
+read.pipe(Transformer).pipe(write);
+
+
+/** Second solution **/
+
+// const readline = require('readline');
+
+// const readlineStream = readline.createInterface({
+//   input: process.stdin,
+// });
+
+// readlineStream.on('line', (input) => {
+//   const reversedString = input.split('').reverse().join('');
+//   console.log(reversedString, '\n');
+// });
