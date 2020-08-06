fa27ab9be91ee43408d639d24f627a00064709e8
Anton Yatskevich <anton.yatskevich@thomsonreuters.com>
2020-08-06 19:01:15 +0300

    reactor according to review

--- task2/index.js
+++ task2/index.js
@@ -1,27 +1,18 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { promisify } from 'util';
 import { createReadStream, createWriteStream } from 'fs';
 import { pipeline } from 'stream';
 import csv from 'csvtojson';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const csvFilePath = path.join(__dirname, './csv/nodejs-hw1-ex1.csv');
-const outputPath = path.join(__dirname, './output.txt');
+const csvFilePath = path.resolve(__dirname, './csv/nodejs-hw1-ex1.csv');
+const outputPath = path.resolve(__dirname, './output.txt');
 
 const readStream = createReadStream(csvFilePath);
 const writeStream = createWriteStream(outputPath);
 
-pipeline(
-  readStream,
-  csv(),
-  writeStream,
-  (err, res) => {
-    if (err) {
-      console.log('ERROR:', err);
-      return;
-    }
-    console.log('SUCCESS');
-  }
-);
+const asyncPipeline = promisify(pipeline);
+
+asyncPipeline(readStream, csv(), writeStream)
+  .then(() => console.log('SUCCESS'))
+  .catch((err) => console.log('ERROR:', err))
+
