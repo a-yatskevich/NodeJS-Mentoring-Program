be163916e8bab8174a0abca0c5e674d12821279e
anton_yatskevich <anton_yatskevich@mail.ru>
2020-10-08 18:20:00 +0300

    use winston to log all requests

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,8 +1,11 @@
+import winston from '../config/winston';
+
 export default function logger(req, res, next) {
     res.on('finish', () => {
         const { method, originalUrl, body } = req;
         const { statusCode } = res;
-        console.log(`[${new Date().toISOString()}] info: METHOD: ${method}, STATUS: ${statusCode}, URL: ${originalUrl}, PARAMS: ${JSON.stringify(body)}`);
+        const message = `METHOD: ${method}, STATUS: ${statusCode}, URL: ${originalUrl}, REQUEST_BODY: ${JSON.stringify(body)}`;
+        winston.info(message);
     });
     next();
 }
